bb927f8d75369f48a96573fa9852ee5e082ab167
rlatjdus0814 <kim01093314735@gmail.com>
2022-04-13 19:55:13 +0900

    jsconfig 수정

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import AppRouter from "components/Router";
-import { authService } from "fbase";
+import AppRouter from "./Router";
+import { authService } from "../fbase";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
