9e6266d2f9f2f598cbb119a389d9b60e77435d27
rlatjdus0814 <kim01093314735@gmail.com>
2022-05-04 21:35:22 +0900

    0504 수업 commit

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,4 @@
-import { authService } from '../fbase';
+import { authService, firebaseInstance } from '../fbase';
 import React, { useState } from 'react';
 
 const Auth = () => {
@@ -34,8 +34,17 @@ const Auth = () => {
     setNewAccount((prev) => !prev);
   }
 
-  const onSocialClick = (event) => {
-    console.log(event.target.name);
+  const onSocialClick = async(event) => {
+    const {target: {name},} = event;
+    let provider;
+    if(name === 'google') {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if (name === 'github') {
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
   }
 
   return (
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Home = () => <span>Home</span>
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  const onChange = (event) => {
+    event.preventDefault();
+    const {
+      target: {value},
+    } =event;
+    setNweet(value);
+  };
+
+  return (
+    <form onSubmit = {onSubmit}>
+      <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+      <input type="submit" value="Nweet" />
+    </form>
+
+  )
+};
 
 export default Home
